e69cec8d7ecc7dba7593ccb47de30e6ece751180
Tommy Werner <wtwerner@gmail.com>
2020-05-08 04:55:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,10 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    print "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   else 
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
